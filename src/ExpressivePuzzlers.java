249d7d385486efc53f096ba7ed10ff7fd3c4cd98
Dave Gonzalez <dgonzalez@thosegonzos.com>
2016-08-07 10:29:11 -0500

    Edit on Ubuntu machine

--- src/ExpressivePuzzlers.java
+++ src/ExpressivePuzzlers.java
@@ -3,10 +3,18 @@ public class ExpressivePuzzlers {
 
 	public static void main(String[] args) 
 	{
+		// Joy of Hex
+		joyOfHex();
+		
 		// Multicast
 		multicast();
 	}
 
+	private static void joyOfHex() 
+	{
+		System.out.println(Long.toHexString(0x100000000L + 0xcafebabe));
+	}
+	
 	private static void multicast() 
 	{
 		System.out.println((int) (char) (byte) -1);
