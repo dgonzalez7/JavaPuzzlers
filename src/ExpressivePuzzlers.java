85c4a86a18ec221f55165e636ca9f31e3822de17
Dave Gonzalez <dgonzalez@thosegonzos.com>
2016-08-07 11:09:40 -0500

    Add isOdd()

--- src/ExpressivePuzzlers.java
+++ src/ExpressivePuzzlers.java
@@ -3,6 +3,12 @@ public class ExpressivePuzzlers {
 
 	public static void main(String[] args) 
 	{
+		// Oddity 
+		for (int i = -10; i <= 10; i++)
+		{
+			System.out.println("i=" + i + " isOdd= " + isOdd(i));
+		}
+		
 		// Joy of Hex
 		joyOfHex();
 		
@@ -10,6 +16,10 @@ public class ExpressivePuzzlers {
 		multicast();
 	}
 
+	public static boolean isOdd(int i) {
+	    return (i & 1) != 0;
+	}
+	
 	private static void joyOfHex() 
 	{
 		System.out.println(Long.toHexString(0x100000000L + 0xcafebabe));
