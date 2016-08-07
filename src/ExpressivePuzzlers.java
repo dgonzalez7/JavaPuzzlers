6c78884b42b1181cd9c6213fd8b54d807e4e506a
Dave Gonzalez <dgonzalez@thosegonzos.com>
2016-08-07 10:01:41 -0500

    Initial code commit

---
+++ src/ExpressivePuzzlers.java
@@ -0,0 +1,15 @@
+
+public class ExpressivePuzzlers {
+
+	public static void main(String[] args) 
+	{
+		// Multicast
+		multicast();
+	}
+
+	private static void multicast() 
+	{
+		System.out.println((int) (char) (byte) -1);
+	}
+
+}
